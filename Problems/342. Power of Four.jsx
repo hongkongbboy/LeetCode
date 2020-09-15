0c973c79665705a7bab64d1047ad533660d995ed
Kin Lay <hongkongbboy@gmail.com>
2020-09-15 04:39:06 -0700

    reverse vowels of a string

--- Problems/342. Power of Four.jsx
+++ Problems/342. Power of Four.jsx
@@ -12,6 +12,17 @@ Output: false
 Follow up: Could you solve it without loops/recursion?
 */
 
+// Method 1
 var isPowerOfFour = function(num) {
     return Math.log10(num) / Math.log10(4) % 1 === 0;
+};
+
+// Method 2
+var isPowerOfFour = function(num) {
+    var power = 0;
+    while (Math.pow(4, power) <= num) {
+        if (Math.pow(4, power) === num) return true;
+        power++;
+    }
+    return false;
 };
---
+++ Problems/345. Reverse Vowels of a String.jsx
@@ -0,0 +1,58 @@
+/*
+Write a function that takes a string as input and reverse only the vowels of a string.
+
+Example 1:
+
+Input: "hello"
+Output: "holle"
+Example 2:
+
+Input: "leetcode"
+Output: "leotcede"
+Note:
+The vowels does not include the letter "y".
+*/
+
+// Method 1
+var reverseVowels = function(s) {
+    var reverseArr = [];
+    var vows = new Set(['a', 'e', 'i', 'o', 'u']);
+    var newS = "";
+    for (var i=0; i<s.length; i++) {
+        if (vows.has(s[i].toLowerCase())) {
+            reverseArr.push(s[i]);
+        }
+    }
+    var len = reverseArr.length - 1;
+    for (var j=0; j<s.length; j++) {
+        if (vows.has(s[j].toLowerCase())) {
+            newS = newS.concat(reverseArr[len]);
+            len--;
+        } else {
+            newS = newS.concat(s[j])
+        }
+    }
+    return newS;
+};
+
+// Method 2
+var reverseVowels = function(s) {
+    var vows = new Set(['a', 'e', 'i', 'o', 'u']);
+    var start = 0;
+    var sArr = s.split('');
+    var end = sArr.length - 1;
+    while (start < end) {
+        while (start < end && !vows.has(sArr[start].toLowerCase())) {
+            start++;
+        }
+        while (start < end && !vows.has(sArr[end].toLowerCase())) {
+            end--;
+        }
+        var temp = sArr[end];
+        sArr[end] = sArr[start];
+        sArr[start] = temp;
+        start++;
+        end--;
+    }
+    return sArr.join('');
+};
